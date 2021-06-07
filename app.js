d2e45af8d7e94f2fb535dda1dfcd5484d627f374
MURALEEDHARAN Vijay Shanker <V9N@dsone.3ds.com>
2021-06-07 21:52:33 +0530

    python and js file added

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
-playAlert = require('alert-sound-notify')
 
 var https = require('follow-redirects').https;
 var fs = require('fs');
 
+let date_ob = new Date();
+console.log(date_ob)
+
+// current date
+// adjust 0 before single digit date
+let date = ("0" + date_ob.getDate()).slice(-2);
+
+// current month
+let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+
+// current year
+let year = date_ob.getFullYear();
+
+let day = date+"-"+month+"-"+year
+console.log(day)
 var options = {
     'method': 'GET',
     'hostname': 'cowin.gov.in',
-    'path': '/api/v2/appointment/sessions/public/findByDistrict?district_id=307&date=07-06-2021',
+    'path': `/api/v2/appointment/sessions/public/findByDistrict?district_id=307&date=${day}`,
     'headers': {
         'Accept-Language': 'hi_IN'
     },
@@ -26,9 +40,8 @@ var req = https.request(options, function (res) {
         var sessions = JSON.parse(body)
 
         sessions['sessions'].forEach(element => {
-            if (element.min_age_limit === 18 && element.available_capacity_dose1 != 0) {
+            if (element.min_age_limit === 18 && element.available_capacity_dose1 == 0) {
                 console.log(element.block_name)
-                playAlert()
             }
         });
 
---
+++ main.py
@@ -0,0 +1,35 @@
+import requests
+from requests import sessions
+import json
+import os
+from twilio.rest import Client
+import datetime
+
+curr_time = datetime.datetime.now()
+curr_date = curr_time.strftime("%d-%m-%Y")
+
+url = "http://cowin.gov.in/api/v2/appointment/sessions/public/findByDistrict?district_id=307&date="+curr_date+"?"
+#print(url)
+payload={}
+headers = {
+  'Accept-Language': 'hi_IN'
+}
+account_sid = os.environ['TWILIO_ACCOUNT_SID']
+auth_token = os.environ['TWILIO_AUTH_TOKEN']
+client = Client(account_sid, auth_token)
+
+while(True):
+    response = requests.request("GET", url, headers=headers, data=payload)
+
+    response_text = response.text
+    session_dict = json.loads(response_text)
+
+    for session in session_dict:
+        for center in session_dict[session]:
+            if (center['available_capacity_dose1'] > 0 and center['vaccine'] == 'COVISHIELD'):
+                print("found vaccine")
+                message = client.messages.create(
+                        body="Vaccine available at"+ center.name+", Pincode: "+center.pincode,
+                        from_='+12406212013',
+                        to='+919495603007'
+                    )
